0a98e43636193362e8ea1b2b1d6840fd2040afff
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-02-02 14:49:56 +0000

    passing by value

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 "use strict";
+
+/*
+const bookings = [];
+
+const createBooking = function (flightNum, numPass = 1, price = 500 * numPass) {
+  //ES5
+  //   numPass = numPass || 1;
+  //   price = price || 199;
+  const booking = {
+    flightNum,
+    numPass,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH123", 2);
+createBooking("LH123", undefined, 200);
+*/
+
+let flight = "LH234";
+const jonas = {
+  name: "Smith",
+  passport: 23418293,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "JK736";
+  passenger.name = "Mr." + passenger.name;
+
+  if (passenger.passport === 23418293) {
+    alert("Check in");
+  } else {
+    alert("Wrong passport");
+  }
+};
+
+checkIn(flight, jonas);
+
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
